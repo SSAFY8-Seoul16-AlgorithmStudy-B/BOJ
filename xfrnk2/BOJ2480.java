2e400e63bdb37ec84cb3ac896b8f8c2c67a9c573
xfrnk2 <xfrnk2@gmail.com>
2022-08-02 08:58:22 +0900

    solve: BOJ2480 주사위 세개

---
+++ xfrnk2/BOJ2480.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		int answer = a == b && a == c ?10000+ a*1000 :
+			a == b || a == c ?1000+ a*100 : b == c ?1000+ b*100 :Math.max(a,Math.max(b, c))*100;
+		System.out.println(answer);
+	}
+}
