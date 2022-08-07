701f63dabc5fc85f55eb9d571c35b784fc1b054c
cheolhun9714 <74818170+cheolhun9714@users.noreply.github.com>
2022-08-07 23:50:14 +0900

    Update Main_s4_2870_함철훈.java
    1주차 틀린 문제 수정&정답

--- cheolhun9714/Main_s4_2870_함철훈.java
+++ cheolhun9714/Main_s4_2870_함철훈.java
@@ -1,28 +1,32 @@
+package cheolhun9714;
+
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Main_s4_2870_함철훈 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N= sc.nextInt();
 		String str = "";
-		int[] num =new int[5000];
+		BigInteger[] num =new BigInteger[5000];
 		int count=0;
 		for (int i=0; i<N;i++) {
 			str = sc.next();
 			for(int j=0;j<str.length();j++) {
 				if(j>0&&str.charAt(j-1)>= 48&&str.charAt(j-1)<=57&&str.charAt(j)>= 48&&str.charAt(j)<=57) {
-					num[count]= num[count]*10+str.charAt(j)-'0';
+					num[count]= num[count].multiply(BigInteger.valueOf(10)).add(BigInteger.valueOf(str.charAt(j)-'0'));
 				}else if(str.charAt(j)>= 48&&str.charAt(j)<=57) {
 					count++;
-					num[count]= str.charAt(j)-'0';
+					num[count]= BigInteger.valueOf(str.charAt(j)-'0');
 				}
 			}
 		}
-		Arrays.sort(num);
-		for(int i =5000-count; i<5000; i++) {
-			System.out.println(num[i]);
+		BigInteger[] new_Array = Arrays.copyOfRange(num,1, count+1);
+		Arrays.sort(new_Array);
+		for(int i =0; i<new_Array.length; i++) {
+			System.out.println(new_Array[i]);
 		}
 		sc.close();
 	}
