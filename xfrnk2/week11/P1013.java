f920f57f79b4e9ef29ae3f51909267a7fc9281fb
xfrnk2 <xfrnk2@gmail.com>
2023-02-07 00:29:43 +0900

    solve: week11

---
+++ xfrnk2/week11/P1013.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+public class Main {
+	static String pattern = "(100+1+|01)+";
+	
+	public static boolean check(String str) {
+
+		return	Pattern.matches(pattern, str)
+				;
+	}
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		
+		for (int i = 0; i < n; i++) {
+			
+			boolean res = check(in.readLine());
+			System.out.println(res ? "YES" : "NO");
+		}
+		
+
+	}
+
+}
